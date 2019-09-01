c779ec1e16398cf8f9525f02458ae287a9b1efc2
Filip Storkan <filip.storkan@gmail.com>
2019-09-01 19:50:41 +0200

    notification

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,10 +1,30 @@
 <template>
   <main>
+    <header-bar scrolled>
+      <div slot="main">
+        <horizontal-groups />
+      </div>
+
+      <user-avatar
+          slot="actions"
+          clickable
+        />
+
+    </header-bar>
   </main>
 </template>
 
 <script>
+import HeaderBar from '@/components/Header'
+import HorizontalGroups from '@/components/Header/HorizontalGroups'
+import UserAvatar from '@/components/Avatars/UserAvatar'
+
 export default {
-  middleware: 'auth'
+  middleware: 'auth',
+  components: {
+    HeaderBar,
+    HorizontalGroups,
+    UserAvatar
+  }
 }
 </script>
